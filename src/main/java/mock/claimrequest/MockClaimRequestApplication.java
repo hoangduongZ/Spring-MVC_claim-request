9d280693d09f1a6b792a5306e7b0adec1ec4312c
HoangDuong <hoangdv.job@gmail.com>
2024-10-09 09:32:54 +0700

    First init project

---
+++ src/main/java/mock/claimrequest/MockClaimRequestApplication.java
@@ -0,0 +1,19 @@
+package mock.claimrequest;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MockClaimRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockClaimRequestApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+}
