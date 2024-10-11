a26ba84e7a5990ca4c6cf1db127a2009a1a61df3
minh <minh@gmail.com>
2024-10-11 14:16:04 +0700

    add code to Employee1

--- src/main/java/mock/claimrequest/entity/Employee.java
+++ src/main/java/mock/claimrequest/entity/Employee.java
@@ -1,4 +1,37 @@
 package mock.claimrequest.entity;
 
+import java.sql.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID) 
+    private UUID id ; 
+    
+    @Column(name="first_name" )
+    private String firstName;
+    
+    @Column(name="last_name" )
+    private String lastName; 
+
+    @Column(name="gender" )
+    private boolean gender ; 
+    
+    @Column(name="date_of_birth" )
+    private Date dob ; 
+    
+    @Column(name="address" )
+    private String address ; 
+    
+    @Column(name="ranked" )
+    private String ranked ;
+    
+    @Column(name="salary" )
+    private String salary ;
 }
